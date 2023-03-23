6e8c5eb785222689ca0fbb0b46634742f1cbdb75
AF <a.harjunpaa02@gmail.com>
2023-03-23 17:55:43 +0200

    Pohja tehty valmiiksi tutoriaalin avulla

--- MatchingGame.cs
+++ MatchingGame.cs
@@ -12,14 +12,107 @@ namespace MatchingGame
 {
     public partial class MatchingGame : Form
     {
+        Label firstClicked = null;
+        Label secondClicked = null;
+
+        Random random = new Random();
+
+        List<string> icons = new List<string>()
+        {
+            "!", "!", "N", "N", ",", ",", "k", "k",
+            "b", "b", "v", "v", "w", "w", "z", "z"
+        };
         public MatchingGame()
         {
             InitializeComponent();
+
+            AssignIconsToSquares();
+        }
+
+        private void AssignIconsToSquares()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if(iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+                    iconLabel.ForeColor = iconLabel.BackColor;
+                    icons.RemoveAt(randomNumber);
+                }
+            }
         }
 
         private void MatchingGame_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled == true)
+                return;
+
+            Label clickedLabel = sender as Label;
+
+            if(clickedLabel != null)
+            {
+                if (clickedLabel.ForeColor == Color.Black)
+                {
+                    return;
+                }
+
+                if(firstClicked == null)
+                {
+                    firstClicked = clickedLabel;
+                    firstClicked.ForeColor = Color.Black;
+
+                    return;
+                }
+
+                secondClicked = clickedLabel;
+                secondClicked.ForeColor = Color.Black;
+
+                CheckForWinner();
+
+                if(firstClicked.Text == secondClicked.Text)
+                {
+                    firstClicked = null;
+                    secondClicked = null;
+                    return;
+                }
+
+                timer1.Start();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+
+            firstClicked = null;
+            secondClicked = null;
+        }
+
+        private void CheckForWinner()
+        {
+            foreach(Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+
+                if(iconLabel  != null)
+                {
+                    if (iconLabel.ForeColor == iconLabel.BackColor)
+                        return;
+                }
+            }
+
+            MessageBox.Show("You won!", "Congratulations");
+            Close();
         }
     }
 }
