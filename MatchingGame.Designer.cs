8a807beca46fe50283c761711fe5a34413909bcc
AF <a.harjunpaa02@gmail.com>
2023-03-23 14:01:31 +0200

    testaan Branchien toimivuutta

--- MatchingGame.Designer.cs
+++ MatchingGame.Designer.cs
@@ -59,7 +59,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(100, 23);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Aapon Oksa";
             // 
             // MatchingGame
             // 
